65a8fa9c4da739320888f7eb35509a305ed17dfe
Michel Domingos <micheldslive@gmail.com>
2023-06-08 11:03:52 -0300

    feat(libs/configs): adding: eslint/jest/storybook/stitches/commitlint/plop/husky/svgr/prettier

---
+++ .generate/plopfile.js
@@ -0,0 +1,41 @@
+module.exports = plop => {
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/app/components/{{dashCase name}}/index.ts',
+        templateFile: 'templates/index.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/app/components/{{dashCase name}}/{{dashCase name}}.tsx',
+        templateFile: 'templates/component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/app/components/{{dashCase name}}/{{dashCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/app/components/{{dashCase name}}/{{dashCase name}}.spec.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      },
+      {
+        type: 'append',
+        path: '../src/app/components/index.ts',
+        pattern: '',
+        template: "export * from './{{dashCase name}}';",
+        separator: ''
+      }
+    ]
+  });
+};
---
+++ jest.config.ts
@@ -0,0 +1,33 @@
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './'
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  roots: ['<rootDir>'],
+  verbose: true,
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  moduleDirectories: ['node_modules', 'src'],
+  collectCoverageFrom: ['src/**/*.test.ts(x)?', '!src/**/*-stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/jest.setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
+  preset: 'ts-jest',
+  setupFiles: ['jest-canvas-mock'],
+  bail: 1,
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss)$': 'babel-jest',
+    '\\.svg$': '<rootDir>/.jest/mocks/svgMock.ts',
+    '@/(.*)': '<rootDir>/$1'
+  }
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+const config = createJestConfig(customJestConfig);
+
+export default config;
