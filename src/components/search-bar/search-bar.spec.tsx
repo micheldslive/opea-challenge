b00990b53ef58eb70ee3e63b2e361bbdfcb415e7
Michel Domingos <micheldslive@gmail.com>
2023-06-12 12:09:55 -0300

    chore: adding language provider

--- src/components/search-bar/search-bar.spec.tsx
+++ src/components/search-bar/search-bar.spec.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { SearchBar } from '.';
+import i18n from '@/i18n';
+import { I18nextProvider } from 'react-i18next';
 
 jest.mock('next/router', () => require('next-router-mock'));
 
 describe('<SearchBar />', () => {
   it('should render the SearchBar', () => {
-    render(<SearchBar />);
+    render(
+      <I18nextProvider i18n={i18n}>
+        <SearchBar />
+      </I18nextProvider>
+    );
 
     expect(screen.getByLabelText(/search-bar/i)).toBeInTheDocument();
   });
